c0b5fa67da75a6407484c974fa2e3735e0142a64
violet2026-jung <dancerg020@gmail.com>
2026-08-31 23:48:07 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -116,6 +116,7 @@ def read_pensiondata():
 
 data = read_pensiondata()
 
+#가입자 수가 가장 많은 회사 출력
 st.title("국민연금 데이터 분석")
 company_name = st.text_input("회사명을 입력해 주세요", placeholder = '검색할 회사명을 입력')
 
@@ -147,4 +148,47 @@ if data and company_name:
 
         col3.text('가입자수 추정')
         col3.markdown(f"`{int(output.iloc[0]['가입자수']):,}` 명")
+
+#동종업계와 비교
+        com_output = daata.compare_company(company_name = company_name)
+        st.dataframe(comp_output.round(0), use_container_width=True)
+
+        st.markdown(f'### 업종 평균 VS {company_name} 비교')
+        # 검색은 회사의 '월급여추정'액과 업종평균을 비교
+        percent_value = info['월급여추정'] / comp_output.iloc[0, 0] * 100 - 100
+        diff_month = abs(comp_output.iloc[0, 0] - info['월급여추정'])  # 월급여추정 액의 차이
+        diff_year = abs(comp_output.iloc[1, 0] - info['연간급여추정'])  # 연간급여추정 액의 차이
+        upordown = '높은' if percent_value > 0 else '낮은'  # %값이 높은지 낮은지에 따른 문구 선택 
+        # 위 결과로 아래에 markdown 으로 출력
+        st.markdown(f"""
+        - 업종 **평균 월급여**는 `{int(comp_output.iloc[0, 0]):,}` 원, **평균 연봉**은 `{int(comp_output.iloc[1, 0]):,}` 원 입니다.
+        - `{company_name}`는 평균 보다 `{int(diff_month):,}` 원, :red[약 {percent_value:.2f} %] `{upordown}` `{int(info['월급여추정']):,}` 원을 **월 평균 급여**를 받는 것으로 추정합니다.
+        - `{company_name}`는 평균 보다 `{int(diff_year):,}` 원 `{upordown}` `{int(info['연간급여추정']):,}` 원을 **연봉**을 받는 것으로 추정합니다.
+        """)   
+
+        fig, ax = plt.subplots(1,2)
+        p1 = ax[0].bar(x=["Average", "Your Company"], height=(comp_output.iloc[0, 0], info['월급여추정']), width=0.7)
+        ax[0].bar_label(p1, fmt='%d')
+        p1[0].set_color('black')
+        p1[1].set_color('red')
+        ax[0].set_title('Monthly Salary')
+
+        p2 = ax[1].bar(x=["Average", "Your Company"], height=(comp_output.iloc[1, 0], info['연간급여추정']), width=0.7)
+        p2[0].set_color('black')
+        p2[1].set_color('red')
+        ax[1].bar_label(p2, fmt='%d')
+        ax[1].set_title('Yearly Salary')
+
+        ax[0].tick_params(axis='both', which='major', labelsize=8, rotation=0)
+        ax[0].tick_params(axis='both', which='minor', labelsize=6)
+        ax[1].tick_params(axis='both', which='major', labelsize=8)
+        ax[1].tick_params(axis='both', which='minor', labelsize=6)
+
+
+        
+        st.pyplot(fig)
+
+
+
+
 
