43098ed33b8392a3a5c18a4afd1655909c9f9933
violet2026-jung <dancerg020@gmail.com>
2026-08-31 23:40:53 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -125,5 +125,26 @@ if data and company_name:
     if len(output) > 0:
         st.subheader(output.iloc[0]['사업장명'])
 
+        info = data.company_info(company_name=company_name)
 
+        st.markdown(
+            f"""
+            - `{info['주소']}`
+            - 업종코드명 `{info['업종코드명']}`
+            - 총 근무자 `{int(info['가입자수']):,}` 명
+            - 신규 입사자 `{info['신규']:,}` 명
+            - 퇴사자 `{info['상실']:,}` 명
+            """
+        ) 
+
+
+        col1, col2, col3 = st.columns(3)
+                col1.text('월급여 추정')
+        col1.markdown(f"`{int(output.iloc[0]['월급여추정']):,}` 원")
+
+        col2.text('연봉 추정')
+        col2.markdown(f"`{int(output.iloc[0]['연간급여추정']):,}` 원")
+
+        col3.text('가입자수 추정')
+        col3.markdown(f"`{int(output.iloc[0]['가입자수']):,}` 명")
 
