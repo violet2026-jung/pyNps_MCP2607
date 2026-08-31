c80d2c7f8dbb2ec56eac1021ae67ae274d7019b1
violet2026-jung <dancerg020@gmail.com>
2026-08-31 23:34:48 +0900

    Enhance UI for 국민연금 데이터 분석
    Add title and company name input for pension data analysis

--- streamlit_main.py
+++ streamlit_main.py
@@ -116,8 +116,14 @@ def read_pensiondata():
 
 data = read_pensiondata()
 
+st.title("국민연금 데이터 분석")
 company_name = st.text_input("회사명을 입력해 주세요", placeholder = '검색할 회사명을 입력')
 
-st.title("국민연금 데이터 분석")
+if data and company_name:
+    output = data.find_company(company_name=company_name)
+
+    if len(output) > 0:
+        st.subheader(output.ilox[0]['사업장'])
+
 
 
